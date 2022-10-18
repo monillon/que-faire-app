6a484515c79800e5f2d6a21a2f3cf1bb227ec8e9
Maxime <monillon@outlook.fr>
2022-10-18 15:24:49 +0200

    Question Page 1 : il faut répondre à toutes les questions

--- app/src/main/java/up/android/quefaire/MainActivity.java
+++ app/src/main/java/up/android/quefaire/MainActivity.java
@@ -2,11 +2,18 @@ package up.android.quefaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "QueFaireApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +29,26 @@ public class MainActivity extends AppCompatActivity {
     public void quit(View v){
         finish();
     }
+
+    public static void toast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void vibrate(Context context, int duration_ms) {
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if(duration_ms < 1)
+            duration_ms = 1;
+        if(v != null && v.hasVibrator()) {
+            // Attention changement comportement avec API >= 26 (cf doc)
+            if(Build.VERSION.SDK_INT >= 26) {
+                v.vibrate(VibrationEffect.createOneShot(duration_ms,
+                        VibrationEffect.DEFAULT_AMPLITUDE));
+            }
+            else {
+                v.vibrate(duration_ms);
+            }
+        }
+        // sinon il n’y a pas de mécanisme de vibration
+    }
+
 }
--- app/src/main/java/up/android/quefaire/QuestionsPage1.java
+++ app/src/main/java/up/android/quefaire/QuestionsPage1.java
@@ -4,20 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class QuestionsPage1 extends AppCompatActivity {
+
+    private RadioButton answer1yes; // oui envie de sortir
+    private RadioButton answer1no; // non envie de sortir
+
+    private TextView answer2; // nb personne
+
+    private RadioButton answer3yes; // oui aimer la nature
+    private RadioButton answer3no; // non aimer la nature
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page1);
+
+        this.answer1yes = findViewById(R.id.answer1yes);
+        this.answer1no = findViewById(R.id.answer1no);
+        this.answer2 = findViewById(R.id.answer2);
+        this.answer3yes = findViewById(R.id.answer3yes);
+        this.answer3no = findViewById(R.id.answer3no);
+
     }
 
     public void next(View v){
+        boolean erreurSaisie = false;
+
         Intent intent = new Intent(this, questions_page2.class);
-        startActivity(intent);
+
+        // toutes les questions doivent avoir une réponse
+        if (!answer1yes.isChecked() && !answer1no.isChecked()) {
+            erreurSaisie = true;
+        }
+
+        if (answer2.getText().toString().isEmpty()) {
+            erreurSaisie = true;
+        }
+
+        if (!answer3yes.isChecked() && !answer3no.isChecked()) {
+            erreurSaisie = true;
+        }
+
+        if (erreurSaisie) {
+            MainActivity.toast(this, "Vous devez remplir tous les champs");
+            MainActivity.vibrate(this, 300);
+
+        } else {
+            startActivity(intent);
+        }
+
     }
+
+    //TODO: Faire on save instance
+    //TODO: Faire on restaure
+    //TODO: Faire intent pour activity suivante
+    //TODO: Faire en sorte que les questions soient répondu pour passer aux suivants
 
     public void back(View v){
         finish();
