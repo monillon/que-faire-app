31262665ae2bbbfdfba2ee88c8dc79beb06ebb3d
Maxime <monillon@outlook.fr>
2022-10-20 11:41:36 +0200

    Récupérer les informations

--- app/src/main/java/up/android/quefaire/Result.java
+++ app/src/main/java/up/android/quefaire/Result.java
@@ -4,15 +4,98 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class Result extends AppCompatActivity {
+
+    private HashMap<String, String> savedData;
+
+    private TextView ResultOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        //récupérer les informations précédentes
+        if (getIntent().getExtras().containsKey("mouille")) {
+            savedData.put("mouille", String.valueOf(getIntent().getIntExtra("mouille", 0)));
+        }
+        if (getIntent().getExtras().containsKey("sensation")) {
+            savedData.put("sensation", String.valueOf(getIntent().getIntExtra("sensation", 0)));
+        }
+        if (getIntent().getExtras().containsKey("animaux")) {
+            savedData.put("animaux", getIntent().getStringExtra("animaux"));
+        }
+
+        //récupérer les informations avant
+        if (getIntent().getExtras().containsKey("answer9Drink")) {
+            savedData.put("answer9Drink", getIntent().getStringExtra("answer9Drink"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Sport")) {
+            savedData.put("answer9Sport", getIntent().getStringExtra("answer9Sport"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Walking")) {
+            savedData.put("answer9Walking", getIntent().getStringExtra("answer9Walking"));
+        }
+        if (getIntent().getExtras().containsKey("answer9ThemePark")) {
+            savedData.put("answer9ThemePark", getIntent().getStringExtra("answer9ThemePark"));
+        }
+        if (getIntent().getExtras().containsKey("answer9EscapeGame")) {
+            savedData.put("answer9EscapeGame", getIntent().getStringExtra("answer9EscapeGame"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Shooping")) {
+            savedData.put("answer9Shooping", getIntent().getStringExtra("answer9Shooping"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Cinema")) {
+            savedData.put("answer9Cinema", getIntent().getStringExtra("answer9Cinema"));
+        }
+        if (getIntent().getExtras().containsKey("answer9VideoGame")) {
+            savedData.put("answer9VideoGame", getIntent().getStringExtra("answer9VideoGame"));
+        }
+        if (getIntent().getExtras().containsKey("answer9BoardGame")) {
+            savedData.put("answer9BoardGame", getIntent().getStringExtra("answer9BoardGame"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Netflix")) {
+            savedData.put("answer9Netflix", getIntent().getStringExtra("answer9Netflix"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Read")) {
+            savedData.put("answer9Read", getIntent().getStringExtra("answer9Read"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Eat")) {
+            savedData.put("answer9Eat", getIntent().getStringExtra("answer9Eat"));
+        }
+        if (getIntent().getExtras().containsKey("answer9sleep")) {
+            savedData.put("answer9sleep", getIntent().getStringExtra("answer9sleep"));
+        }
+        if (getIntent().getExtras().containsKey("answer9Museum")) {
+            savedData.put("answer9Museum", getIntent().getStringExtra("answer9Museum"));
+        }
+        if (getIntent().getExtras().containsKey("mood")) {
+            savedData.put("mood", getIntent().getStringExtra("mood"));
+        }
+        if (getIntent().getExtras().containsKey("age")) {
+            savedData.put("age", getIntent().getStringExtra("age"));
+        }
+        if (getIntent().getExtras().containsKey("payante")) {
+            savedData.put("payante", getIntent().getStringExtra("payante"));
+        }
+        if (getIntent().getExtras().containsKey("interetCulture")) {
+            savedData.put("interetCulture", getIntent().getStringExtra("interetCulture"));
+        }
+        if (getIntent().getExtras().containsKey("typeCulture")) {
+            savedData.put("typeCulture", getIntent().getStringExtra("typeCulture"));
+        }
+        if (getIntent().getExtras().containsKey("sortirCeSoir")) {
+            savedData.put("sortirCeSoir", getIntent().getStringExtra("sortirCeSoir"));
+        }
+        if (getIntent().getExtras().containsKey("nbPersonne")) {
+            savedData.put("nbPersonne", getIntent().getStringExtra("nbPersonne"));
+        }
+
     }
 
     public void restart(View v){
